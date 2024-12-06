a96e1d130dc49883207dae3c6e1e9118eae881b9
Riley Chang <rileycki3333@gmail.com>
2024-12-06 11:59:39 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tkngaejcpi.github.io',
 
-	integrations: [],
+	integrations: [sitemap()],
 
 	markdown: {
 		syntaxHighlight: false,
